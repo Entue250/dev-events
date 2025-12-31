91ec7e6351719b02631fa3921c6bb5a9c46b7e09
Entue250 <edouardoniyomugabo@gmail.com>
2025-12-31 19:49:06 +0200

    Add Next Configuration file

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,32 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "res.cloudinary.com",
+//       },
+//     ],
+//   },
+//   async rewrites() {
+//     return [
+//       {
+//         source: "/ingest/static/:path*",
+//         destination: "https://us-assets.i.posthog.com/static/:path*",
+//       },
+//       {
+//         source: "/ingest/:path*",
+//         destination: "https://us.i.posthog.com/:path*",
+//       },
+//     ];
+//   },
+//   // This is required to support PostHog trailing slash API requests
+//   skipTrailingSlashRedirect: true,
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -6,6 +35,7 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
+        pathname: "/**",
       },
     ],
   },
